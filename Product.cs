22051e566da52f5182b1c32a35c5f3566df0e8ef
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-06-08 08:51:29 +0900

    초기 커밋.

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassAdv3_2
+{
+    internal class Product
+    {
+        private string name;
+        private int price;
+
+        public Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        ~Product()
+        {
+            Console.WriteLine(this.name + " 소멸자 호출");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using ClassAdv3_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAdv3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("아이스크림", 1000);
+            Product product3 = new Product("케이크", 1000);
+
+
+            Box box = new Box(10, 10);
+            box.Width = 100; 
+            box.Height = 100;
+            Console.WriteLine("Area: " + box.GetArea());
+            try
+            {
+                box.Width = -10; 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
